aaec7a95721c343c2f114665d2f74fc71c126008
Rene G <r.gromeier@gmail.com>
2023-06-24 11:41:05 +0200

    feat: simple Zustand store implemented

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,18 @@
 // import {useState} from 'react';
 import './App.css';
+import {MinonStore} from './utils/zustand.store';
 
 function App() {
+	const {minon, increase, decrease} = MinonStore();
+
 	return (
 		<>
 			<h1>This is Minon Mentor</h1>
+			<div>
+				<button onClick={() => increase(1)}>+ minons</button>
+				<span>{minon}</span>
+				<button onClick={() => decrease(1)}>- minons</button>
+			</div>
 		</>
 	);
 }
---
+++ frontend/src/utils/zustand.store.tsx
@@ -0,0 +1,13 @@
+import {create} from 'zustand';
+
+interface MinonState {
+	minon: number;
+	increase: (by: number) => void;
+	decrease: (by: number) => void;
+}
+
+export const MinonStore = create<MinonState>()((set) => ({
+	minon: 0,
+	increase: (by) => set((state) => ({minon: state.minon + by})),
+	decrease: (by) => set((state) => ({minon: state.minon - by})),
+}));
