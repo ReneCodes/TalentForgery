dff092cb71d40b472517a92409b0b14770ec59ce
Caian Keyes <caiankeyes@gmail.com>
2023-06-27 11:34:27 +0100

    feat: question listt swaps between questions

--- frontend/src/Components/Create/Question.tsx
+++ frontend/src/Components/Create/Question.tsx
@@ -2,15 +2,24 @@ import {FC} from 'react';
 import {QuestionType} from '../../utils/types';
 
 const Question: FC<{question: QuestionType}> = ({question}) => {
-  return <div className='question'>
-    <p className='question_title'>{question.question}</p>
-    <div className='question_line'></div>
-    <ul className='options_list'>
-      {question.options.map((option, index) => (
-        <li className={option === question.answer ? 'correct' : 'incorrect'} key={index}>{option}</li>
-      ))}
-    </ul>
-  </div>
-}
+  return (
+    question && (
+      <div className="question">
+        <p className="question_title">{question.question}</p>
+        <div className="question_line"></div>
+        <ul className="options_list">
+          {question.options.map((option, index) => (
+            <li
+              className={option === question.answer ? 'correct' : 'incorrect'}
+              key={index}
+            >
+              {option}
+            </li>
+          ))}
+        </ul>
+      </div>
+    )
+  );
+};
 
 export default Question;
--- frontend/src/Components/Create/QuestionList.tsx
+++ frontend/src/Components/Create/QuestionList.tsx
@@ -1,11 +1,14 @@
 import { useEffect, useState } from "react";
-import AddQuestion from "./AddQuestion";
 import Question from "./Question";
 import { QuestionType } from '../../utils/types';
 import './Create.css';
+import ArrowForwardIosTwoToneIcon from '@mui/icons-material/ArrowForwardIosTwoTone';
+import ArrowBackIosNewTwoToneIcon from '@mui/icons-material/ArrowBackIosNewTwoTone';
+import AddQuestion from "./AddQuestion";
 
 const QuestionList = () => {
   const [questions, setQuestions] = useState<QuestionType[]>([]);
+  const [counter, setCounter] = useState(1);
 
   useEffect(() => {
     const obj = {
@@ -13,17 +16,35 @@ const QuestionList = () => {
       options: ['cheese', 'ham', 'mcDonalds'],
       answer: 'mcDonalds'
     }
-
     setQuestions([obj])
   }, [])
 
-  return <div>
-    <ul>
-      {questions.map((question) => (
-        <Question question={question} />
-      ))}
-    </ul>
-    <AddQuestion />
+  const handleLeft = () => {
+    if (counter === 1) {
+      setCounter(questions.length + 1)
+    } else {
+      setCounter((num) => num-1)
+    }
+  }
+
+  const handleRight = () => {
+    if (counter === questions.length + 1) {
+      setCounter(1);
+    } else {
+      setCounter((num) => num+1)
+    }
+  }
+
+  return <div className="question_list">
+    <div className="question_top">
+      {counter === questions.length + 1 ? <AddQuestion /> : <Question question={questions[0]} />}
+      <h1 className="index">{counter}/{questions.length + 1}</h1>
+    </div>
+    <div className="question_bar">
+      <ArrowBackIosNewTwoToneIcon onClick={handleLeft} className="arrow" />
+      <button className="delete">Delete</button>
+      <ArrowForwardIosTwoToneIcon onClick={handleRight}  className="arrow" />
+    </div>
   </div>
 }
 
