ce5d1a23ae30bc108941289c2e16c7bba4f3add8
Caian Keyes <caiankeyes@gmail.com>
2023-07-02 09:55:31 +0100

    test: cypress create test, chore: before each set up for each test

--- cypress/e2e/profile.cy.js
+++ cypress/e2e/profile.cy.js
@@ -1,5 +1,18 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('profile', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:5173');
+
+    cy.get('button').eq(0).click();
+
+    cy.get('input').eq(0).type('caiankeyes@gmail.com');
+    cy.get('input').eq(1).type('User1_123');
+
+    cy.get('button').eq(1).click();
+
+    cy.contains('Dashboard').should('be.visible');
+
+    cy.contains('Profile').click();
   })
+
+  it('passes', () => {})
 })
