cefd6e2c5311a32d11bd62f1e728755f6c76941f
Caian Keyes <caiankeyes@gmail.com>
2023-07-03 16:49:09 +0100

    feat: tutorial info dialog

--- frontend/src/Components/Create/TutorialCard.tsx
+++ frontend/src/Components/Create/TutorialCard.tsx
@@ -1,16 +1,29 @@
 import { Card } from "@mui/material";
-import { FC } from "react";
+import { FC, useState } from "react";
 import { DataType } from "../../utils/types";
 import ImagePreview from "./ImagePreview";
+import TutorialInfo from "./TutorialInfo";
 
 const TutorialCard: FC<{tutorial: DataType}> = ({tutorial}) => {
+  const [open, setOpen] = useState(false);
 
-  return <Card className="tutorial_card">
-    <h3>{tutorial.title}</h3>
-    <div className="image_card">
-      <ImagePreview showPreview={true} imageData={tutorial.image_url} />
-    </div>
-  </Card>
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return <div>
+    <Card className="tutorial_card" onClick={handleClickOpen}>
+      <h3>{tutorial.title}</h3>
+      <div>
+        <ImagePreview showPreview={true} imageData={tutorial.image_url} />
+      </div>
+    </Card>
+    <TutorialInfo open={open} onClose={handleClose} tutorial={tutorial}/>
+  </div>
 }
 
 export default TutorialCard;
--- frontend/src/Components/Create/TutorialInfo.tsx
+++ frontend/src/Components/Create/TutorialInfo.tsx
@@ -1,5 +1,27 @@
-const TutorialInfo = () => {
+import { Dialog, DialogTitle } from "@mui/material";
+import { FC} from "react";
+import { DataType } from "../../utils/types";
+import VideoPreview from "./VideoPreview";
 
+interface TutorialInfoProps {
+  open: boolean;
+  onClose: () => void
+  tutorial: DataType;
+}
+
+const TutorialInfo: FC<TutorialInfoProps> = ({open, onClose, tutorial}) => {
+  return (
+    <div>
+      <Dialog onClose={onClose} open={open}>
+        <DialogTitle>Genral Info Tutorial</DialogTitle>
+        <div>
+          <h2>{tutorial.title}</h2>
+          <h3>{tutorial.description}</h3>
+        </div>
+        <VideoPreview showPreview={true} videoData={tutorial.video_url} />
+      </Dialog>
+    </div>
+  );
 }
 
 export default TutorialInfo;
