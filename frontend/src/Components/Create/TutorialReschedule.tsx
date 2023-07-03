f5069f82bae2ca9e95055e0ce06f910fcc8a461b
Caian Keyes <caiankeyes@gmail.com>
2023-07-03 19:35:54 +0100

    feat: reschedule now

--- frontend/src/Components/Create/TutorialReschedule.tsx
+++ frontend/src/Components/Create/TutorialReschedule.tsx
@@ -1,6 +1,8 @@
-import { Dialog, DialogTitle } from "@mui/material";
-import { FC} from "react";
+import { Button, Dialog, DialogTitle, Divider } from "@mui/material";
+import { FC, useState} from "react";
 import { DataType } from "../../utils/types";
+import QuestionList from "./NewQuestionList";
+import Schedule from "./Schedule";
 import VideoPreview from "./VideoPreview";
 
 interface TutorialInfoProps {
@@ -10,6 +12,29 @@ interface TutorialInfoProps {
 }
 
 const TutorialReshedule: FC<TutorialInfoProps> = ({open, onClose, tutorial}) => {
+  const [scheduleOpen, setScheduleOpen] = useState(false);
+  const [formInfo, setFormInfo] = useState(tutorial);
+
+  const handleScheduleOpen = () => {
+    setScheduleOpen(true);
+  };
+
+  const handleScheduleClose = () => {
+    setScheduleOpen(false);
+    onClose();
+  };
+
+  const handleScheduleData = (data: {startDate: string, endDate: string}) => {
+    setFormInfo((res) => {
+      return {
+        ...res,
+        access_date: data.startDate,
+        due_date: data.endDate,
+      }
+    });
+    console.log(formInfo);
+  }
+
   return (
     <div>
       <Dialog onClose={onClose} open={open}>
@@ -21,7 +46,22 @@ const TutorialReshedule: FC<TutorialInfoProps> = ({open, onClose, tutorial}) => 
           </div>
           <VideoPreview showPreview={true} videoData={tutorial.video_url} />
         </div>
+        {tutorial.question_ids.length > 0 && <div>
+          <Divider />
+          <QuestionList questions={tutorial.question_ids} />
+          </div>
+        }
+        <Divider />
+        <div className='quiz_line schedule_line'>
+          <Button variant='contained' onClick={onClose}>Cancel</Button>
+          <Button variant='contained' onClick={handleScheduleOpen}>Schedule</Button>
+        </div>
       </Dialog>
+      <Schedule 
+        open={scheduleOpen}  
+        onClose={handleScheduleClose}
+        onData={handleScheduleData}
+      />
     </div>
   );
 }
