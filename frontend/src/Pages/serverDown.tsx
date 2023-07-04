8c220540407401015dfd1054374e0813b9090c88
Marko Grahovac <m_grahovac@yahoo.com>
2023-07-04 15:32:25 +0200

    server_down component

---
+++ frontend/src/Pages/serverDown.tsx
@@ -0,0 +1,16 @@
+const ServerDown = () => {
+  return (
+    <div style={{ textAlign: "center" }}>
+      <h1>Server is Down</h1>
+      <p>It seems like our server is currently down.</p>
+      <p>Please try again later.</p>
+      <img
+        src="https://th.bing.com/th/id/OIP.vh0BMVlp2WmgpUZWvzGkxQHaEu?w=256&h=180&c=7&r=0&o=5&dpr=1.1&pid=1.7"
+        alt="Minions working on construction"
+        style={{ maxWidth: "100%", height: "auto" }}
+      />
+    </div>
+  );
+};
+
+export default ServerDown;
