132b4fad74ac52529fa7fb363ac1075a7970d6b2
Caian Keyes <caiankeyes@gmail.com>
2023-07-04 10:15:03 +0100

    feat: import is a dropdown

---
+++ frontend/src/Components/Create/ImportQuestion.tsx
@@ -0,0 +1,5 @@
+const ImportQuestion = () => {
+  
+}
+
+export default ImportQuestion;
--- frontend/src/Components/Create/TutorialForm.tsx
+++ frontend/src/Components/Create/TutorialForm.tsx
@@ -1,6 +1,7 @@
 import TextField from '@mui/material/TextField';
-import Button from '@mui/material/Button';
 import { useState, FC, useEffect } from 'react';
+import { TutorialTagStore } from '../../utils/zustand.store';
+import TagsList from './TagsList';
 
 interface TutorialFormComp {
   getData: boolean,
@@ -10,18 +11,11 @@ interface TutorialFormComp {
 const TutorialForm: FC<TutorialFormComp> = ({getData, onData}) => {
   const [title, setTitle] = useState('');
   const [description, setDesciption] = useState('');
-  const [tag, setTag] = useState('');
-  const [tags, setTags] = useState<string[]>([]);
 
-  const handleAdd = () => {
-    if(tag) {
-      setTags((res) => [...res, tag]);
-      setTag('');
-    }
-  }
+  const { selctedTags } = TutorialTagStore()
 
   useEffect(() => {
-    if(getData) onData({title, description, tags})
+    if(getData) onData({title, description, tags: selctedTags})
   }, [getData])
 
   return <div className='form'>
@@ -39,23 +33,9 @@ const TutorialForm: FC<TutorialFormComp> = ({getData, onData}) => {
       value={description}
       onChange={(e) => setDesciption(e.target.value)}
     />
-    <TextField 
-      id="standard-basic" 
-      label="tags" 
-      value={tag}
-      onChange={(e) => setTag(e.target.value)}
-    />
+    <TagsList />
     <div className='form_add'>
-      <Button onClick={handleAdd} variant="contained">Add Tag</Button>
     </div>
-    <ul className='options_list'>
-      {tags.map((item, index) => (
-        <li 
-          className='option'
-          key={index}>{item}
-        </li>
-      ))}
-    </ul>
   </div>
 }
 
