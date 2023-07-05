1e9b7a48bb15d2390ba0ae1f8005870088ba5b47
Caian Keyes <caiankeyes@gmail.com>
2023-07-05 17:43:59 +0100

    test: cypress other dialogs

--- cypress/e2e/create.cy.js
+++ cypress/e2e/create.cy.js
@@ -30,54 +30,53 @@ describe('Create tutorial', () => {
 
     cy.get('input').eq(5).type('hi');
     cy.get('input').eq(6).type('hi');
-    cy.get('button').eq(12).click();
+    cy.get('button').eq(13).click();
     cy.get('input').eq(7).type('bye');
-    cy.get('button').eq(12).click();
+    cy.get('button').eq(13).click();
     cy.get('input').eq(6).click();
 
     cy.get('input').eq(9).type(1);
 
-    cy.get('button').eq(15).click();
+    cy.get('button').eq(16).click();
 
     cy.contains('Begin').should('be.visible');
     cy.contains('end').should('be.visible');
   })
 
-  // it('should send an alert if title is not included', () => {
-  //   cy.get('button').eq(5).click();
+  it('can submit a form without questions', () => {
+    cy.get('button').eq(3).click();
 
-  //   cy.on('window:alert',(t)=>{
-  //     expect(t).to.contains('form information missing');
-  //   })
-  // })
+    cy.get('input').eq(0).type('1');
+    cy.get('textarea').eq(0).type('2');
 
-  // it('should send an alert if description is not included', () => {
-  //   cy.get('input').eq(0).type('How to put out a fire');
-  //   cy.get('button').eq(5).click();
+    const videoFile = 'little_kitten_playing_his_toy_mouse (360p).mp4';
+    cy.get('input[type="file"]').eq(0).attachFile(videoFile, { force: true });
 
-  //   cy.on('window:alert',(t)=>{
-  //     expect(t).to.contains('form information missing');
-  //   })
-  // });
+    const imageFile = '/Screenshot 2023-05-13 at 08.55.17.png';
+    cy.get('input[type="file"]').eq(1).attachFile(imageFile, { force: true });
 
-  // it('should send an alert if test length is not included', () => {
-  //   cy.get('input').eq(0).type('How to put out a fire');
-  //   cy.get('input').eq(1).type('How to');
-  //   cy.get('button').eq(5).click();
+    cy.get('button').eq(13).click();
 
-  //   cy.on('window:alert',(t)=>{
-  //     expect(t).to.contains('form information missing');
-  //   })
-  // });
+    cy.contains('Begin').should('be.visible');
+    cy.contains('end').should('be.visible');
+  })
 
-  // it('should send an alert if test length is more then questions.length', () => {
-  //   cy.get('input').eq(0).type('How to put out a fire');
-  //   cy.get('input').eq(1).type('How to');
-  //   cy.get('input').eq(3).type('7');
-  //   cy.get('button').eq(5).click();
+  it('can see the details about a tutorial', () => {
+    cy.get('button').eq(5).click();
 
-  //   cy.on('window:alert',(t)=>{
-  //     expect(t).to.contains('must have more or equal questions to the length');
-  //   })
-  // });
+    cy.contains('Tutorial Reshedule').should('be.visible');
+
+    cy.get('button').eq(12).click();
+
+    cy.contains('Create').should('be.visible');
+
+    cy.get('button').eq(5).click();
+
+    cy.contains('Tutorial Reshedule').should('be.visible');
+
+    cy.get('button').eq(13).click();
+
+    cy.contains('Begin').should('be.visible');
+    cy.contains('end').should('be.visible');
+  })
 })
