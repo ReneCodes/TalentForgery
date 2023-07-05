f4e55f5278bc52ad5827fb0a07dc096fd5d6b91c
Rene G <r.gromeier@gmail.com>
2023-07-05 02:15:23 +0200

    feat: time tutorial access depend on time

---
+++ frontend/src/Components/Cards/TimedTutorial/TimedStartWatchTutorial.tsx
@@ -0,0 +1,270 @@
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Slide from '@mui/material/Slide';
+import {Box, Card, DialogActions, DialogContent, DialogContentText, DialogTitle} from '@mui/material';
+import {TransitionProps} from '@mui/material/transitions';
+// Components / Types
+import TimedTotorialVideo from './TimedTotorialVideo';
+import TimedQuizzList from './TimedQuizzList';
+import {SmallVideoData} from '../../../@types/Types';
+// icons
+import CloseIcon from '@mui/icons-material/Close';
+import {WhiteTooltip, YellowTooltip} from '../../Tooltips/CustomTooltips';
+
+// Dialog Visual Transistion
+const Transition = React.forwardRef(function Transition(
+	props: TransitionProps & {
+		children: React.ReactElement;
+	},
+	ref: React.Ref<unknown>
+) {
+	return (
+		<Slide
+			direction="up"
+			ref={ref}
+			{...props}
+		/>
+	);
+});
+
+interface TutorialQuestions {
+	question: {
+		question: string;
+		options: string[];
+		answer: string;
+		question_id: string;
+	};
+}
+
+interface TimedWatchTutorialProps {
+	videoData: SmallVideoData['videoData'];
+	tutorialQuestions: TutorialQuestions[];
+	accessTime: string | undefined;
+	accessDate: string | undefined;
+	currentDateInMs: number | undefined;
+	accessDateInMs: number | undefined;
+	createCurrentDate: () => void;
+}
+
+const TimedStartWatchTutorial: React.FC<TimedWatchTutorialProps> = ({
+	videoData,
+	tutorialQuestions,
+	accessDate,
+	accessTime,
+	currentDateInMs,
+	accessDateInMs,
+	createCurrentDate,
+}) => {
+	// local States
+	const [open, setOpen] = React.useState(false);
+	const [openAlert, setOpenAlert] = React.useState(false);
+	const [videoToWatch, setVideoToWatch] = React.useState(true);
+	const [quizzToDo, setQuizzToDo] = React.useState(false);
+	const [quizzDone, setQuizzDone] = React.useState(false);
+
+	const handleClickOpen = async () => {
+		setOpen(true);
+	};
+
+	const handleClose = () => {
+		setOpen(false);
+	};
+
+	const handleAlertOpen = () => {
+		setOpenAlert(true);
+	};
+	const handleAlertClose = () => {
+		setOpenAlert(false);
+	};
+	const handleAbortTutorial = () => {
+		console.log('User Aborted Tutorial');
+		handleClose();
+		setOpenAlert(false);
+	};
+
+	// console.log('new render');
+	// console.log('currentDateTime', currentDateTime);
+	// console.log('accessDate', accessDate);
+	// console.log('accessTime', accessTime);
+
+	return (
+		<div>
+			{accessDateInMs && currentDateInMs && accessDateInMs <= currentDateInMs ? (
+				<Button
+					variant="contained"
+					sx={styles.access_btn}
+					aria-label="Start Tutorial"
+					onClick={handleClickOpen}>
+					Start Tutorial
+				</Button>
+			) : (
+				<YellowTooltip
+					title="Click to Check Time"
+					placement="bottom"
+					describeChild
+					arrow>
+					<Button
+						variant="contained"
+						aria-label="no access yet"
+						sx={styles.access_denied_btn}
+						onClick={createCurrentDate}>
+						<Typography>{`Unlocked at`}</Typography>
+						<Typography>{`${accessDate} ${accessTime}`}</Typography>
+					</Button>
+				</YellowTooltip>
+			)}
+			{/* <Typography>{currentDateTime}</Typography> */}
+			<Dialog
+				fullScreen
+				open={open}
+				onClose={handleAlertOpen}
+				TransitionComponent={Transition}>
+				<AppBar sx={{position: 'relative', backgroundColor: 'primary.main'}}>
+					<Toolbar>
+						<Typography
+							sx={{ml: 2, flex: 1, fontWeight: 'bold', fontSize: '32px'}}
+							variant="h6"
+							component="div">
+							{videoData.title}
+						</Typography>
+						<IconButton
+							edge="start"
+							color="inherit"
+							onClick={handleAlertOpen}
+							aria-label="close">
+							<CloseIcon />
+						</IconButton>
+						<Dialog
+							open={openAlert}
+							onClose={handleAlertClose}
+							aria-labelledby="abort-tutorial"
+							aria-describedby="alert-dialog-description">
+							<DialogTitle id="abort-tutorial">Alert Aborting Tutorial</DialogTitle>
+							<DialogContent>
+								<DialogContentText id="alert-dialog-description">
+									You are about to about the ufinished tutorial and will lose all progress.
+								</DialogContentText>
+								<DialogContentText>Are you sure you want to abort tutorial?</DialogContentText>
+							</DialogContent>
+							<DialogActions>
+								<Button
+									variant="outlined"
+									onClick={handleAlertClose}
+									sx={{
+										fontWeight: 'bold',
+										borderColor: 'green.main',
+										color: 'green.900',
+										':hover': {
+											backgroundColor: 'green.900',
+											borderColor: 'green.900',
+											color: 'white.main',
+										},
+									}}>
+									Continue Watching
+								</Button>
+								<Button
+									variant="outlined"
+									onClick={handleAbortTutorial}
+									autoFocus
+									sx={{
+										fontWeight: 'bold',
+										borderColor: 'red.main',
+										color: 'red.900',
+										':hover': {
+											backgroundColor: 'red.900',
+											borderColor: 'red.900',
+											color: 'white.main',
+										},
+									}}>
+									Abort Tutorial
+								</Button>
+							</DialogActions>
+						</Dialog>
+					</Toolbar>
+				</AppBar>
+				{videoToWatch && (
+					<TimedTotorialVideo
+						videoData={videoData}
+						setVideoToWatch={setVideoToWatch}
+						setQuizzToDo={setQuizzToDo}
+					/>
+				)}
+				{quizzToDo && (
+					<Box sx={{display: 'flex', justifyContent: 'center', mx: 'auto', mt: '100px'}}>
+						<TimedQuizzList
+							setQuizzDone={setQuizzDone}
+							setQuizzToDo={setQuizzToDo}
+							tutorialQuestions={tutorialQuestions}
+							videoData={videoData}
+						/>
+					</Box>
+				)}
+				{quizzDone && (
+					<Box sx={{display: 'flex', justifyContent: 'center', mx: 'auto', mt: '100px'}}>
+						<Card sx={styles.quizz_done}>
+							<Typography variant="h2">Thank you!</Typography>
+							<Typography variant="overline">Your quizz has been send to get analysed</Typography>
+							<Typography variant="overline">You will get the result in no time.</Typography>
+							<Button
+								variant="contained"
+								sx={{
+									backgroundColor: 'secondary.main',
+									color: 'gray.900',
+									py: 1,
+									px: 3,
+									':hover': {
+										backgroundColor: 'secondary.900',
+									},
+								}}
+								onClick={handleClose}
+								autoFocus>
+								Done
+							</Button>
+						</Card>
+					</Box>
+				)}
+			</Dialog>
+		</div>
+	);
+};
+export default TimedStartWatchTutorial;
+
+/** @type {import("@mui/material").SxProps} */
+const styles = {
+	quizz_done: {
+		display: 'flex',
+		flexDirection: 'column',
+		gap: 2,
+		justifyContent: 'center',
+		textAlign: 'center',
+		mx: 3,
+		width: {xs: '100%', sm: '500px', md: '600px'},
+		minWidth: '300px',
+		height: '100%',
+		minHeight: '400px',
+		p: 3,
+	},
+	access_btn: {
+		display: 'flex',
+		flexDirection: 'column',
+		backgroundColor: 'white.main',
+		color: 'gray.900',
+		':hover': {
+			backgroundColor: 'secondary.main',
+		},
+	},
+	access_denied_btn: {
+		display: 'flex',
+		flexDirection: 'column',
+		backgroundColor: 'gray.900',
+		color: 'white.main',
+		':hover': {
+			backgroundColor: 'red.900',
+		},
+	},
+};
--- frontend/src/Components/Cards/TimedTutorial/TimedTutorialCard.tsx
+++ frontend/src/Components/Cards/TimedTutorial/TimedTutorialCard.tsx
@@ -6,30 +6,75 @@ import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
-import theme from '../../../config/theme';
+// Components / Types / Utils
 import TimedVideoInfo from './TimedVideoInfo';
-import TimedWatchTutorial from './TimedWatchTutorial';
+import TimedStartWatchTutorial from './TimedStartWatchTutorial';
 import {SmallVideoData} from '../../../@types/Types';
-
+import {niceDate, niceTime} from '../../../utils/niceDate';
 // Icons
 import AssignmentIcon from '@mui/icons-material/Assignment';
 import {TutorialStore} from '../../../utils/zustand.store';
 import {getQuestions} from '../../../services/Api.service';
+import {YellowTooltip, BlackTooltip} from '../../Tooltips/CustomTooltips';
 
 // TODO: get the TS Prop Type right
+
+// {
+// 	"tutorial_id": "2425c212-eafd-4fdb-bcbf-bd986f62c543",
+// 	"title": "Chemical Brothers",
+// 	"video_url": "16884961792711688496179271people55s.mp4",
+// 	"questions_id": [
+// 			"9bb2f29e-2098-4a41-9193-01ffcafc83cb",
+// 			"ed9c5391-2684-4d28-8bc8-3e91a018f4ac",
+// 			"7ab3657b-80fa-49cd-ae66-81664c2cb269"
+// 	],
+// 	"description": "People Managment",
+// 	"questions_shown": 2,
+// 	"access_date": "Tue, 4 Jul 2023 23:00:00 GMT",
+// 	"tags": [
+// 			"HR",
+// 			"FIRE"
+// 	],
+// 	"due_date": "Thu, 6 Jul 2023 23:00:00 GMT",
+// 	"video_thumb": "16884961793301688496179330people_thumb.png"
+// }
+
 const TimedTutorialCard = ({videoData}: SmallVideoData) => {
-	// Color Theme
-	const {secondary} = theme.palette;
 	// Zustand Stores
 	const {video_thumb_base_url} = TutorialStore();
 	//
-	const {title, video_thumb, tutorial_id} = videoData;
+	const {title, video_thumb, tutorial_id, access_date, due_date} = videoData;
 	// Local Stores
 	const [tutorialQuestions, setTutorialQuestions] = React.useState([]);
+	const [accessDate, setAccessDate] = React.useState<string>();
+	const [accessTime, setAccessTime] = React.useState<string>();
+	const [niceDueDate, setNiceDueDate] = React.useState<string>();
+	const [nicDueTime, setNiceDueTime] = React.useState<string>();
+	const [currentDateInMs, setCurrentDateInMs] = React.useState<number>();
+	const [accessDateInMs, setAccessDateInMs] = React.useState<number>();
+
+	// console.log('videoData', videoData);
+
+	const createCurrentDate = async () => {
+		console.log('check Time');
+		const milliseconds = Date.now();
+		setCurrentDateInMs(milliseconds);
+	};
 
 	React.useEffect(() => {
 		fetchQuestions();
+		createCurrentDate();
 	}, []);
+
+	React.useEffect(() => {
+		setAccessDate(niceDate(access_date));
+		setAccessTime(niceTime(access_date));
+		setNiceDueDate(niceDate(due_date));
+		setNiceDueTime(niceTime(due_date));
+		const date = new Date(access_date);
+		const asseccInMS = date.getTime();
+		setAccessDateInMs(asseccInMS);
+	}, [due_date]);
 
 	function fetchQuestions() {
 		getQuestions({tutorial_id}, setTutorialQuestions);
@@ -39,7 +84,7 @@ const TimedTutorialCard = ({videoData}: SmallVideoData) => {
 	function allowedAccess() {}
 
 	return (
-		<Card sx={{maxWidth: 345, minWidth: 300, position: 'relative'}}>
+		<Card sx={{maxWidth: 345, minWidth: 300, position: 'relative', mb: 2}}>
 			<CardMedia
 				// VIDEO THUMB
 				component="img"
@@ -49,23 +94,50 @@ const TimedTutorialCard = ({videoData}: SmallVideoData) => {
 			/>
 			<CardContent>
 				<CardActions sx={{position: 'absolute', top: 100, left: '50%', transform: 'translate(-50%,-50%)'}}>
-					<TimedWatchTutorial
+					<TimedStartWatchTutorial
 						videoData={videoData}
 						tutorialQuestions={tutorialQuestions}
+						accessDate={accessDate}
+						accessTime={accessTime}
+						currentDateInMs={currentDateInMs}
+						accessDateInMs={accessDateInMs}
+						createCurrentDate={createCurrentDate}
 					/>
 				</CardActions>
 				<Typography
 					gutterBottom
 					variant="h5"
-					component="div">
+					component="div"
+					sx={{cursor: 'default'}}>
 					{title}
 				</Typography>
+
 				<Box sx={{display: 'flex', gap: '10px', justifyContent: 'space-between', alignItems: 'center'}}>
-					{/* TODO: insert time from fetched Data */}
-					07. Jul 23 - 9:00
+					<YellowTooltip
+						title="Due Date"
+						placement="bottom"
+						describeChild
+						arrow>
+						<Typography
+							variant="overline"
+							sx={{
+								cursor: 'default',
+								borderBottom: '1px solid',
+								borderColor: 'transparent',
+								':hover': {borderBottom: '1px solid', borderColor: 'secondary.main'},
+							}}>
+							{`${niceDueDate} - ${nicDueTime}`}
+						</Typography>
+					</YellowTooltip>
 					<TimedVideoInfo videoData={videoData} />
 					<Box sx={{display: 'flex'}}>
-						<AssignmentIcon sx={{color: secondary.main}} />
+						<BlackTooltip
+							title="Has Quizz"
+							placement="bottom"
+							describeChild
+							arrow>
+							<AssignmentIcon sx={{color: 'secondary.main'}} />
+						</BlackTooltip>
 					</Box>
 				</Box>
 			</CardContent>
